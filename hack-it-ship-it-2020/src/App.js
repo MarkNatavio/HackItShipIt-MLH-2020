e1ce88ce4f826e0a5de65527ea09c1522ff48e08
MarkNatavio <43160780+MarkNatavio@users.noreply.github.com>
2020-07-17 17:59:23 -0400

    Update App.js

--- hack-it-ship-it-2020/src/App.js
+++ hack-it-ship-it-2020/src/App.js
@@ -5,9 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <body>
         <h2>Want to help out on the strugles?</h2>
+        <hr></hr>
         <h5>We would like you to let us know about your concerns and interactions (if any) that you have had with Covid-19.</h5>
         <button>Click Me</button>
+      </body>
       </header>
     </div>
   );

fc02d7bf4050402d4192335859a710178cf098b4
Mark Natavio <marknatavio@Marks-MacBook-Pro.local>
2020-07-17 16:36:24 -0400

    add files

---
+++ hack-it-ship-it-2020/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Want to help out on the strugles?</h2>
+        <h5>We would like you to let us know about your concerns and interactions (if any) that you have had with Covid-19.</h5>
+        <button>Click Me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
